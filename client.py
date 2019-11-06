8018f33cadb150cdaf305463e8eed3944bd292f8
SaumitraChaskar <somichaskar@gmail.com>
2019-11-06 19:03:28 +0530

    initiate

--- client.py
+++ client.py
@@ -1,39 +1,64 @@
 import socket
 import os
+
 def Main():
-    host = '172.16.31.106'
+    host = '127.0.0.1'
     port = '5000'
+
+    # create a socket 
     s = socket.socket()
+
+    # connect the socket to the Server socket
     s.connect((host,int(port)))
+
+    # ask for filename
     filename = input('Filename? ')
     if filename != 'q':
         filename = bytes(filename, 'utf-8')
+
+        # send file name over to the Server socket
         s.send(filename)
+
+        # Start recieving data
         data = s.recv(1024)
-        print(data)
-        print(data[:6])
+        
+        # check if file EXISTS
         check = b'EXISTS'
         if data[:6] == check:
-            #statinfo = os.stat(filename)
             filesize = int(data[6:])
+
+            # ask for download
             message = input('File Exists,' + str(filesize)+ 'Bytes, downloaded(Y/N) ?')
             if message == 'Y' :
                 ok = 'OK'
                 ok = bytes(ok,'utf-8')
+
+                # send message over the socket
                 s.send(ok)
+
+                # create new file to store the downloaded file
                 f = open('new_' + filename.decode() ,'wb')
                 data = s.recv(1024)
-                print(data)
                 f.write(data)
                 totalRecv = len(data)
+                print("****" + str(totalRecv) + "****")
+
+    			# recieve file
                 while totalRecv < filesize:
                     data = s.recv(1024)
                     totalRecv += len(data)
                     f.write(data)
                     print('{0:.2f}'.format((totalRecv/float(filesize)*100)) + '%Done')
                 print('Download Complete')
+                
+                done = "Done"
+                s.send(bytes(done,'utf-8'))
         else:
+        	# if file is not found
             print('File does not exist')
+
+        # close the socket
         s.close()
+
 if __name__ == '__main__':
-    Main()        
+    Main()
--- server.py
+++ server.py
@@ -1,15 +1,26 @@
 import socket 
 import threading
 import os
+
 def RetrFile(name,sock):
+	# recieve filename
     filename = sock.recv(1024)
+    print("****** filename ******")
+    print(filename)
+
+    # check if file exists
     if os.path.isfile(filename):
+    	# send 'EXISTS' message and file size
         stringSend = 'EXISTS' + str(os.path.getsize(filename))
         stringSend = bytes(stringSend,'utf-8')
         sock.send(stringSend)
+
+        # get User response
         userResponse = sock.recv(1024)
         print(userResponse)
         check = b'OK'
+
+        # if OK response send File via socket
         if userResponse[:2] == check:
             with open(filename,'rb') as f:
                 bytesToSend = f.read(1024)
@@ -18,20 +29,37 @@ def RetrFile(name,sock):
                     bytesToSend = f.read(1024)
                     sock.send(bytesToSend)
         else:
+        	# send ERROR message if Not 
             sock.send(bytes('ERR','utf-8'))
+
+        # close socket connection
+        status = sock.recv(1024)
+        print(status)
         sock.close()
+
+
 def Main():
-    host = '172.16.31.106'
+	# Host address and Port
+    host = '127.0.0.1'
     port = '5000'
+
+    # create socket object and bind IP and PORT
     s = socket.socket()
     s.bind((host,int(port)))
+
+    # maximum backlogged requests not yet accepted by the server
     s.listen(5)
     print('Server Started.')
     while True:
+    	# connection and adress are returned 
         c, addr = s.accept()
         print('Client connected ip:<' + str(addr)+ '>')
+        # new thread for every connection
         t = threading.Thread(target = RetrFile,args = ('retrThread',c))
         t.start()
+
+    # close the socket connection
     s.close()
+
 if __name__ == '__main__':
     Main()
